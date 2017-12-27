9c72adead830859fb981002771ac4c6d6f38f60e
Tanglong <13685759344@163.com>
2017-12-27 22:59:08 +0800

    function

--- python_basic/python_function.py
+++ python_basic/python_function.py
@@ -28,3 +28,29 @@ def hello (name = 'World!'):
 
 hello()
 hello("Python!")
+
+# variable parameters
+def vars(arg, *args):
+    print("arg=", arg)
+    for x in args:
+        print(x, end=',')
+    print()
+
+vars("para")
+vars(1,3,5,7,9)
+
+# anonymous function(lambda)
+mysum = lambda a, b: a + b
+
+print("mysun: ", mysum(3,4))
+
+# return
+def mysum2(a,b):
+    return a + b
+
+print("mysum2:", mysum2(5,7))
+
+# key words parameters
+def print_str(str):
+    print(str)
+print_str(str = "Hello Python function...")
