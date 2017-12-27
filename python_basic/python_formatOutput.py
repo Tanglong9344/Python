3eebeb3dc2b8f0088b06d57c75098e5d745f9657
Tanglong <13685759344@163.com>
2017-12-27 16:11:25 +0800

    format output

--- python_basic/python_formatOutput.py
+++ python_basic/python_formatOutput.py
@@ -1,44 +1,26 @@
-# --1
-for j in range(1, 6):
-    print('* ' * j)
-print()
+# format output
+ch = 'A'
+print("%c" %ch) #字符
 
-# --2
-for i in range(0, 5):
-    for j in range(0, 5):
-        print('* ', end='')
-    print()
-print()
+str = "Python"
+print("%s" %str) #字符串
 
-# --3
-for i in range(0, 5):
-    print('* ' * 5)
-print()
-
-# --4
-for i in range(0, 5):
-    for j in range(0, i + 1):
-        print('* ', end=''),
-    print()
-print()
-
-# Mutiply table
-for i in range(1, 10):
-    for j in range(1, 10):
-        print("%d*%d=%2d\t" % (j, i, i * j), end='')
-    print()
-
-# number-1
-for i in range(1, 4):
-    for j in range(1, 5):
-        print("%d" % i * j)
-
-# number-2
-for i in range(1, 4):
-    for j in range(1, 5):
-        print("%d" % (i * j))
-
-# number-3
-for i in range(1, 4):
-    for j in range(1, 5):
-        print("%d*%d=%2d" % (i, j, i * j))
+i = -1024
+print("%d" %i)   #十进制整数
+print("%5d" %i)  #指定宽度
+print("%i" %i)   #带符号的十进制整数
+print("%u" %i)   #无符号的十进制整数
+print("%f" %i)   #十进制浮点数
+print("%.2f" %i) #指定精度
+print("%3.2f" %i)#指定宽度和精度
+print("%e" %i)   #科学计数
+print("%E" %i)   #科学计数
+print("%o" %i)   #八进制
+print("%X" %i)   #十六进制
+print("%x" %i)   #十六进制
+print("%#o" %i)  #八进制，添加前导符号
+print("%#X" %i)  #十六进制，添加前导符号
+print("%#x" %i)  #十六进制，添加前导符号
+print("%-10d" %i) #左对齐
+print("%+10d" %i) #右对齐
+print("%010d" %i) #空位补0
