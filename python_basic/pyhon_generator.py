c0dccdfe0a8bf90abad2edc5a7347500f9184b41
Tanglong <13685759344@163.com>
2017-12-28 16:44:03 +0800

    generator

---
+++ python_basic/pyhon_generator.py
@@ -0,0 +1,27 @@
+# generator
+import sys
+
+def fibonacci(n): # generator function
+   a, b, counter = 1, 1, 1
+   while True:
+      if (counter > n):
+         return
+      yield a
+      a, b = b, a + b
+      counter += 1
+f = fibonacci(10) # f is iterator object
+
+while True:
+   try:
+      print (next(f), end=" ")
+   except StopIteration:
+      break
+print()
+
+str = "Tanglong"
+
+def revese_str(srt):
+    length = len(str)
+    for i in range(length - 1, -1, -1):
+        yield str[i]
+print(revese_str(str))
