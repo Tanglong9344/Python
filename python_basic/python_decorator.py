e72bba3e760b15d1ebbed79e7272c99b39086144
Tanglong <13685759344@163.com>
2017-12-28 18:15:18 +0800

    decorator

---
+++ python_basic/python_decorator.py
@@ -0,0 +1,78 @@
+# python decorator
+# Everything in python is object
+# meta-programming
+
+# method as a object in python
+def m_str(str):
+    print(str)
+
+m_str("Python...")
+m1 = m_str
+m2 = m1
+m1("m1, Python...")
+m2("m2, Python...")
+
+print("name:\n",m_str.__name__, m1.__name__, m2.__name__)
+print("code:\n",m_str.__code__,"\n", m1.__code__,"\n", m2.__code__)
+
+# high-order function
+def add(x,y):
+    return x + y
+
+def substract(x,y):
+    return x -y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+def operator(fun,x,y):
+    return fun(x,y)
+
+print(operator(add,3,4))
+print(operator(substract,3,4))
+print(operator(multiply,3,4))
+print(operator(divide,3,4))
+
+# decorator-receive a function and add something new to it
+def fun_old():
+    print("Before decorated...")
+
+def fun_new(fun):
+    def decorator():
+        fun()
+        print("After decorator...")
+    # return new function
+    return decorator
+
+# decorator--1
+# before
+fun_old()
+# decorator
+fun_old = fun_new(fun_old)
+#after
+fun_old()
+
+# decorator--2
+@fun_new     # (@ + decorator function)
+def fun_old():
+    print("Before decorated...")
+#after
+fun_old()
+
+# decorator parameter-if divided by zero ,will return None
+def smart_divide(func):
+   def inner(a,b):
+      if b == 0:
+         print("Can't divided by 0...")
+         return
+      return func(a,b)
+   return inner
+
+@smart_divide
+def divide(a,b):
+    return a/b
+
+print(divide(3,0))
