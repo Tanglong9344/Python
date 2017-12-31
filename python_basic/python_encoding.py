6d950446e03c326b9b81e518c56febc1203c2160
Tanglong <13685759344@163.com>
2017-12-31 10:22:13 +0800

    Update

--- python_basic/python_encoding.py
+++ python_basic/python_encoding.py
@@ -1,2 +1,9 @@
 import sys
 print(sys.getdefaultencoding())# default encoding
+
+s = "中文"
+print("s:",s)
+s_encode = s.encode("utf-8")
+print("s_encode:",s_encode)
+s_decode = s_encode.decode("utf-8")
+print("s_decode:",s_decode)
