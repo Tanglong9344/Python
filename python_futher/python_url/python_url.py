a1415c89e70909304abf7ac2c47886d6fd156f49
Tanglong <13685759344@163.com>
2018-01-03 22:53:57 +0800

    urllib

--- python_futher/python_url/python_url.py
+++ python_futher/python_url/python_url.py
@@ -5,7 +5,6 @@ import re
 
 web = urllib.request.urlopen('http://www.baidu.com')
 content = web.read()
-re.sub('\n',content,'')
-re.sub('\t',content,'')
 fp = open("baidu.html",'wb')
 fp.write(content)
+fp.close()
