9602e1308453abb6936968236e90fc98ed3970c6
Tanglong <13685759344@163.com>
2018-01-16 22:16:17 +0800

    count repeat word

--- python_futher/python_file/count_repeat_word.py
+++ python_futher/python_file/count_repeat_word.py
@@ -15,7 +15,7 @@ print("Version-2.0:")
 import string
 with open('ss.txt', 'r') as text:
     words = [raw_word.strip(string.punctuation).lower() for raw_word in text.read().split()]
-    words_index = set(words) # change  number of the repeat words to 1
+    words_index = set(words) # change  number of the repeat words to
     counts_dict = {index: words.count(index) for index in words_index}
     for word in sorted(counts_dict, key=lambda x: counts_dict[x], reverse=True):
         print('{}:{} times'.format(word, counts_dict[word]), end=', ')
