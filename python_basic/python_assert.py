fc20ab30c20fe00d02d111021c3a491fbe6d0c79
Tanglong <13685759344@163.com>
2018-01-16 16:24:45 +0800

    assert

---
+++ python_basic/python_assert.py
@@ -0,0 +1,9 @@
+# Python assertion
+
+print("Before Assertion,True...")
+assert 1 > 0
+print("After Asertion,True...")
+
+print("Before Assertion,False...")
+assert 1 < 0
+print("After Asertion,False...")
