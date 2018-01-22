9d188fbc5262ff1c8673500c3ee243fa83c58452
Tanglong <13685759344@163.com>
2018-01-22 22:39:28 +0800

    eval and exec

---
+++ python_futher/python_eval_exec.py
@@ -0,0 +1,11 @@
+# The eval statement is used to evaluate
+# valid Python expressions which are stored in a string.
+# A simple example is shown below.
+print(eval('2+3*4'))
+
+# The exec statement is used to execute Python statements
+# which are stored in a string or file.
+# For example,we can generate a string containing Python code
+# at runtime and then execute these statements using the exec statement.
+# A simple example is shown below.
+exec("print('Hello Exec...')")
