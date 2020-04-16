dabd6d1f5fcc4fbcb0a9d29c282b80fc00a09b17
Tanglong <13685759344@163.com>
2020-04-16 15:48:15 +0800

    configuration

---
+++ python_futher/configuration/configTest.py
@@ -0,0 +1,8 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.conf')
+name = config.get("param", "name")
+age = config.getint("param", "age")
+
+print('name:%s,age:%d'%(name,age))
