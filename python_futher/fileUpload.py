a744b480835f7820fcf0f017b91735fd5e9bca64
程多 <107499708@qq.com>
2020-04-24 09:14:18 +0800

    file upload

---
+++ python_futher/fileUpload.py
@@ -0,0 +1,15 @@
+import requests
+
+upload_url = 'http://localhost:8089/api/uploadFile.do'
+print('upload url:', upload_url)
+# 打开文件
+filePath = 'E:\\pythonProject\\paperCheck\\test.pdf'
+with open(filePath, 'rb') as f:
+    files = {'file': ('test.pdf', open(filePath, 'rb'))}
+    try:
+        print('xxxxxxx')
+        response = requests.post(url=upload_url, files=files)
+        print("response:", response.json())
+        response.close()
+    except Exception as e:
+        print("upload error:", e)
